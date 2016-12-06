84fee3f2a347792071509daf7b3afafb13c9c7fc
leshareva <reva@mysmaxom.com>
2016-12-07 02:17:16 +0600

    some works with main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { TaskComponent } from './task/task.component';
+import { ChatComponent } from './task/chat/chat.component';
+import { DetailComponent } from './task/detail/detail.component';
+import { TasksListComponent } from './task/tasks-list/tasks-list.component';
+import { UserpicComponent } from './userpic/userpic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TaskComponent,
+    ChatComponent,
+    DetailComponent,
+    TasksListComponent,
+    UserpicComponent
   ],
   imports: [
     BrowserModule,
